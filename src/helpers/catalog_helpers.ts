d33a13d3e288b13f4af187dc351a6de1c9a20c9c
Muhammad Al-Tahhan <me@mhd-altahan.com>
2024-10-27 13:06:21 +0300

    Add the ability to change page size

--- src/helpers/catalog_helpers.ts
+++ src/helpers/catalog_helpers.ts
@@ -6,6 +6,8 @@ const getData = (options: HelperOptions) => {
      * HelperOptions.hash is used to receive data in name/value pairs
      * and it is useful to provide structured data to a helper.
      * 
+     * A Hash is a dictionary-like collection of unique keys and their values.
+     * 
      * HelperOptions.data provides access to the context data, and `root`
      * containts the data from the template that invoked the helper.
      */
@@ -36,7 +38,6 @@ export const pageButtons = (options: HelperOptions) => {
 
     // Create content for each data page.
     for (let i = 1; i <= pageCount; i++) {
-        // Supplement the shared pagination data with `index` and `selected`.
         /**
          * Each call for `fn` will add everything between the helpers tags
          * using the context provided to it.
@@ -44,6 +45,8 @@ export const pageButtons = (options: HelperOptions) => {
          * This options hash contains a function (options.fn) that behaves like
          * a normal compiled Handlebars template. Specifically, the function
          * will take a context and return a String.
+         * 
+         * Supplement the shared pagination data with `index` and `selected`.
          */
         output += options.fn({
             page,
@@ -52,6 +55,19 @@ export const pageButtons = (options: HelperOptions) => {
             selected: i === page
         });
     }
+
+    return output;
+}
+
+export const pageSizeOptions = (options: HelperOptions) => {
+    const { pageSize } = getData(options);
+    let output = "";
+    [3, 6, 9].forEach(size => {
+        output += options.fn({
+            size,
+            selected: pageSize === size ? "selected" : ""
+        });
+    });
 
     return output;
 }
