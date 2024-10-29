a0f7ad38286abc5d2a714d18344a63c15e98bf0a
Muhammad Al-Tahhan <me@mhd-altahan.com>
2024-10-29 18:14:52 +0300

    Create data models for orders

---
+++ src/data/order_models.ts
@@ -0,0 +1,24 @@
+import { Customer } from "./customer_models";
+
+export interface Order {
+    id?: number;
+    customer?: Customer;
+    selections?: ProductSelection[];
+    address?: Address;
+    shipped: boolean;
+}
+
+export interface ProductSelection {
+    id?: number;
+    productId?: number;
+    quantity: number;
+    price: number;
+}
+
+export interface Address {
+    id?: number;
+    street: string;
+    city: string;
+    state: string;
+    zip: string;
+}
