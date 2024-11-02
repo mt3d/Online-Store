211aa0f1726e5e246f98257f6576f4d6e1da40bb
Muhammad Al-Tahhan <me@mhd-altahan.com>
2024-11-02 16:35:56 +0300

    Add templates for editing products

--- src/helpers/admin_helpers.ts
+++ src/helpers/admin_helpers.ts
@@ -1,2 +1,6 @@
 export const buttonClass = (btn: string, mode: string) =>
     btn === mode ? "btn-secondary" : "btn-outline-secondary";
+
+export const disabled = (val: any) => val == "ID" ? "disabled" : "";
+
+export const selected = (val1: any, val2: any) => val1 == val2 ? "selected" : "";
