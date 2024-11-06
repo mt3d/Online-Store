51c60738d3fd1d01c790dc4b916f48468867fff4
Muhammad Al-Tahhan <me@mhd-altahan.com>
2024-11-06 10:59:52 +0300

    Restrict access to adminstration features

--- src/authentication.ts
+++ src/authentication.ts
@@ -8,48 +8,82 @@ import { customer_repository } from "./data";
 const clientID = getSecret("GOOGLE_CLIENT_ID");
 const clientSecret = getSecret("GOOGLE_CLIENT_SECRET");
 const callbackURL: string = getConfig("auth:openauth:redirectionUrl");
+const authCallbackURL: string = getConfig("admin:openauth:redirectionUrl");
 
 declare global {
     namespace Express {
         // This user object is added to authenticated requests by Passport.
-        interface User extends Customer { }
+        interface User extends Customer {
+            adminUser?: boolean;
+        }
     }
 }
 
 export const createAuthentication = (app: Express) => {
-    passport.use(new GoogleStrategy({
+    passport.use("admin-auth", new GoogleStrategy(
+        {
         clientID,
         clientSecret,
-        callbackURL,
+        callbackURL: authCallbackURL,
         scope: ["email", "profile"],
         state: true
-    }, async (accessToken: string, refreshToken: string, profile: Profile, callback: VerifyCallback) => {
-        /**
-         * The callback function receives an access token, which can be used to make
-         * API queries, and a refresh token, which can be used to obtain a new access token.
-         * 
-         * The last parameter is invoked once the user's data is ready.
-         */
-        const emailAddr = profile.emails?.[0].value ?? "";
-        const customer = await customer_repository.storeCustomer({
-            name: profile.displayName,
-            email: emailAddr,
-            federatedId: profile.id
-        });
+        },
+        (accessToken: string, refreshToken: string, profile: Profile, callback: VerifyCallback) => {
+            return callback(null, {
+                name: profile.displayName,
+                email: profile.emails?.[0].value ?? "",
+                federatedId: profile.id,
+                adminUser: true
+            });
+        }
+    ));
 
-        const { id, name, email } = customer;
-        return callback(null, { id, name, email });
-    }));
+    passport.use(new GoogleStrategy(
+        {
+            clientID,
+            clientSecret,
+            callbackURL,
+            scope: ["email", "profile"],
+            state: true
+        },
+        async (accessToken: string, refreshToken: string, profile: Profile, callback: VerifyCallback) => {
+            /**
+             * The callback function receives an access token, which can be used to make
+             * API queries, and a refresh token, which can be used to obtain a new access token.
+             * 
+             * The last parameter is invoked once the user's data is ready.
+             */
+            const emailAddr = profile.emails?.[0].value ?? "";
+            const customer = await customer_repository.storeCustomer({
+                name: profile.displayName,
+                email: emailAddr,
+                federatedId: profile.id
+            });
+
+            const { id, name, email } = customer;
+            return callback(null, { id, name, email });
+        }
+    ));
 
     // Serialize the user data into a session.
     passport.serializeUser((user, callback) => {
-        callback(null, user.id);
+        /**
+         * There is no persistent data store for administration user details, so the
+         * serializeUser and deserializeUser functions have been modified
+         * to serialize the entire User object in the session, but only when the
+         * adminUser property is true .
+         */
+        callback(null, user.adminUser ? JSON.stringify(user) : user.id);
     });
 
-    passport.deserializeUser((id: number, callbackFunc) => {
-        customer_repository.getCustomer(id).then(user => {
-            callbackFunc(null, user);
-        });
+    passport.deserializeUser((id: number | string, callbackFunc) => {
+        if (typeof id == "string") {
+            callbackFunc(null, JSON.parse(id));
+        } else {
+            customer_repository.getCustomer(id).then(user => {
+                callbackFunc(null, user);
+            });
+        }
     });
 
     /**
--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -1,35 +1,72 @@
-import { Express, Router } from "express";
+import { Express, Router, Request, Response, NextFunction } from "express";
 import { createAdminCatalogRoutes } from "./admin_catalog_routes";
 import { createAdminOrderRoutes } from "./admin_order_routes";
+import passport from "passport";
+import { getConfig } from "../../config";
+
+const users: string[] = getConfig("admin:users", []);
 
 export const createAdminRoutes = (app: Express) => {
     app.use((req, res, next) => {
         res.locals.layout = false; // Disable the default layout for the template engine.
+        res.locals.user = req.user;
         next();
     });
 
+    app.get("/admin/signin", (req, res) => res.render("admin/signin"));
+    app.post("/admin/signout", (req, res) => req.logOut(() => { res.redirect("/admin/signin") }));
+    app.get("/admin/google", passport.authenticate("admin-auth"));
+    app.get("/auth-signin-google", passport.authenticate("admin-auth", {
+        successRedirect: "/admin/products",
+        keepSessionInfo: true
+    }));
+
+    const authCheck = (r: Request) => {
+        console.log(users);
+        console.log(r.user);
+        console.log(r.user?.email);
+        return users.find(u => r.user?.email === u)
+    }
+
+    const apiAuth = (req: Request, res: Response, next: NextFunction) => {
+        if (!authCheck(req)) {
+            return res.sendStatus(401);
+        }
+        next();
+    }
+
     const cat_router = Router();
     createAdminCatalogRoutes(cat_router);
-    app.use("/api/products", cat_router);
+    app.use("/api/products", apiAuth, cat_router);
 
     const order_router = Router();
     createAdminOrderRoutes(order_router);
-    app.use("/api/orders", order_router);
+    app.use("/api/orders", apiAuth, order_router);
 
-    app.get("/admin", (req, res) => res.redirect("admin/products"));
+    const userAuth = (req: Request, res: Response, next: NextFunction) => {
+        if (!authCheck(req)) {
+            console.log("redirecting to sign in");
+            return res.redirect("/admin/signin");
+        }
+        next();
+    }
 
-    app.get("/admin/products", (req, res) => {
+    app.get("/admin", userAuth, (req, res) => res.redirect("admin/products"));
+
+    app.get("/admin/products", userAuth, (req, res) => {
         res.locals.content = "/api/products/table";
         res.render("admin/admin_layout");
     });
 
-    app.get("/admin/products/edit/:id", (req, res) => {
+    app.get("/admin/products/edit/:id", userAuth, (req, res) => {
         res.locals.content = `/api/products/edit${req.params.id}`;
         res.render("admin/admin_layout");
     });
 
-    app.get("/admin/orders", (req, res) => {
+    app.get("/admin/orders", userAuth, (req, res) => {
         res.locals.content = "/api/orders/table";
         res.render("admin/admin_layout");
     });
+
+
 }
